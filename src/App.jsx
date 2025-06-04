59b6754b7f939c3918fb6fb62accc11e50ee35c7
ZahaAnass <zahaanass277@gmail.com>
2025-06-04 20:35:22 +0100

    add search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
-import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
 
 function App() {
-  const movieNumber = 1;
 
   return (
     <>
-      {movieNumber === 1 && <MovieCard movie={{title: "Tim's Film", release_date: "2024"}}/>}
-      <MovieCard movie={{title: "Joes Film", release_date: "2020"}}/>
+      <Home/>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
-import MovieCard from "../components/MovieCard"
+import MovieCard from "../components/MovieCard.jsx"
+import { useState } from "react"
 
 function Home(){
+
+    const [searchQuery, setSearchQuery] = useState("")
+
     const movies = [
         {id: 1, title: "The Dark Knight", releaseDate: "2008"},
         {id: 2, title: "Inception", releaseDate: "2010"},
@@ -8,12 +12,26 @@ function Home(){
         {id: 4, title: "The Matrix", releaseDate: "1999"}
     ]
 
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert("Hello")
+        setSearchQuery("")
+    }
+
     return(
         <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" placeholder="Search for movies ..."
+                        className="search-input" value={searchQuery} 
+                        onChange={(e) => setSearchQuery(e.target.value)}/>
+                <button type="submit" className="search-button">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </form>
             <div className="movies-grid">
-                {movies.map(movie => {
+                {movies.map(movie => (
                     <MovieCard movie={movie} key={movie.id}/>
-                })}
+                ))}
             </div>
         </div>
     )
