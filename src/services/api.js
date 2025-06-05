5d75b281b3ed5ebb947ab2e2bdf50ff6ccf59378
ZahaAnass <zahaanass277@gmail.com>
2025-06-05 14:41:03 +0100

    add api from themoviedb.org

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+// This is the API key from themoviedb.org
+const API_KEY = "a7461f2ca7c829de722ae359345d86a5"
+const API_BASE_URL = "https://api.themoviedb.org/3"
+
+export const getMovies = async () => {
+    const response = await fetch(`${API_BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${API_BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+        query
+    )}`)
+    const data = await response.json()
+    return data.results
+}
